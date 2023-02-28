3f3524eb3d7acdbb1b7f3e088ea495131a7c8660
flavius25 <flavius.m1996@yahoo.com>
2023-02-28 22:16:02 +0100

    -we have now 3D output with Parameters_BSpline.txt. -parameters_bspline_multires_MR not enough, but maybe we can check it later. -added RegistrationParametersFiles from Slicer (open-source)

--- Flavius/program_elastix.py
+++ Flavius/program_elastix.py
@@ -43,36 +43,46 @@ if __name__ == "__main__":
     fixed_image_path = "./TrainingData/p102/mr_bffe.mhd"
     fixed_image_seg_path = "./TrainingData/p102/prostaat.mhd"
     moving_image_path = "./TrainingData/p120/mr_bffe.mhd"
-
+	
+	# anytime you want to see visualize different, you can comment this line to avoid waiting too much time
     jacobian_determinant_path = elastix_registration(fixed_image_path, fixed_image_seg_path, moving_image_path, ELASTIX_PATH, TRANSFORMIX_PATH)
 
 
     # look at the result image (create the path)
-    result_path = os.path.join('results', 'result.0.tiff')
-    result_path_transf = os.path.join('results', 'result.tiff')
-    transformed_bsplined_image = imageio.imread(result_path)
-    transformed_bsplined_segmentation = imageio.imread(result_path_transf)
+    # result_path = os.path.join('results', 'result.0.tiff')
+    # result_path_transf = os.path.join('results', 'result.tiff')
+    # transformed_bsplined_image = imageio.imread(result_path)
+    # transformed_bsplined_segmentation = imageio.imread(result_path_transf)
+    result_path = os.path.join('results', 'result.0.mhd')
+    result_path_transf = os.path.join('results', 'result.mhd')
 
+    transformed_bsplined_image_on = sitk.ReadImage(result_path)
+    transformed_bsplined_image = sitk.GetArrayViewFromImage(transformed_bsplined_image_on)
+    transformed_bsplined_segmentation_on = sitk.ReadImage(result_path_transf)
+    transformed_bsplined_segmentation = sitk.GetArrayViewFromImage(transformed_bsplined_segmentation_on)
+    slice_nr = 30
     # display images and graphs
     fig, axarr = plt.subplots(1, 5)
-    axarr[0].imshow(fixed_image[30, :, :], cmap='gray')
-    axarr[0].imshow(fixed_image_seg[30, :, :], alpha=0.5*(fixed_image_seg[30, :, :]>0), cmap='Purples')
+    plt.suptitle('Slice number: ' + str(slice_nr), fontsize=16)
+    axarr[0].imshow(fixed_image[slice_nr, :, :], cmap='gray')
+    axarr[0].imshow(fixed_image_seg[slice_nr, :, :], alpha=0.5*(fixed_image_seg[slice_nr, :, :]>0), cmap='Purples')
     axarr[0].set_title('Fixed image')
-    axarr[1].imshow(moving_image[30, :, :], cmap='gray')
+    axarr[1].imshow(moving_image[slice_nr, :, :], cmap='gray')
     axarr[1].set_title('Moving image')
-    axarr[2].imshow(transformed_bsplined_segmentation, cmap='gray')
+    axarr[1].imshow(transformed_bsplined_segmentation[slice_nr, :, :], alpha=0.8*(transformed_bsplined_segmentation[slice_nr, :, :]>0), cmap='Purples')
+    axarr[2].imshow(transformed_bsplined_segmentation[slice_nr, :, :], cmap='gray')
     axarr[2].set_title('Resulted B-splined segmentation')
-    axarr[3].imshow(transformed_bsplined_image, cmap='gray')
+    axarr[3].imshow(transformed_bsplined_image[slice_nr, :, :], cmap='gray')
     axarr[3].set_title('Resulted B-splined image')
-    axarr[4].imshow(imageio.imread(jacobian_determinant_path.replace('dcm', 'tiff')))
-    axarr[4].set_title('Jacobian\ndeterminant')
+    # axarr[4].imshow(imageio.imread(jacobian_determinant_path.replace('dcm', 'tiff'))[30,:,:])
+    # axarr[4].set_title('Jacobian\ndeterminant')
     # plt.axis('off')
 
     plt.tight_layout()
 
     fig2, axarr = plt.subplots(1, 1)
     # Open the logfile into the dictionary log
-    for i in range(5):
+    for i in range(4):
         log_path = os.path.join('results', 'IterationInfo.0.R{}.txt'.format(i))
         log = elastix.logfile(log_path)
         # Plot the 'metric' against the iteration number 'itnr'
